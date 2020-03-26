cd5236fd933c46d012684ba318bf1b90a0d8dcdd
Kartik Goel <goel.kartik39@gmail.com>
2020-03-26 23:55:30 +0530

    Updated TextArea

--- client/src/components/reusable/TextArea.js
+++ client/src/components/reusable/TextArea.js
@@ -6,7 +6,7 @@ const TextAreaGroup = ({ name, placeholder, value, info, onChange }) => {
     <div className="form-group">
       <small className="form-text text-muted text-left small ">{info}</small>
       <textarea
-        className={"form-control form-control-lg textarea_common"}
+        className="form-control form-control-lg textarea_common"
         placeholder={placeholder}
         name={name}
         value={value}
