9a85042c91f43efc7e9d776421ee0d90fb3cae8a
Kartik Goel <goel.kartik39@gmail.com>
2020-03-26 23:54:23 +0530

    Updated Landing.js

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -4,13 +4,13 @@ import UploadForm from "./UploadForm";
 import mainImage from "../mainpage.svg"
 const Landing = (props) => {
   return (
-    <div className="container-fluid ">
+    <div className="container-fluid landing ">
       <div className="row">
         <div className="col-md-6 landing_container_left">
             <UploadForm />
         </div>
         <div className="col-md-6 landing_container_right">
-          <h1 className="main_heading">Store and share your data.</h1>
+          <h1 className="main_heading text-center">Store and share your data.</h1>
           <center><img src={mainImage}></img></center>
         </div>
       </div>
