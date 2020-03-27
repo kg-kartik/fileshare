c269ec63c9d4836cacd9b8412ae266c516197805
Kartik Goel <goel.kartik39@gmail.com>
2020-03-27 21:54:04 +0530

    Added config for fileupload

---
+++ client/src/config.js
@@ -0,0 +1,8 @@
+const production = "false";
+
+// const apiUrl = production ? "https://domain.com/api" : "https://localhost:3000/api";
+
+
+const apiUrl = "http://localhost:5000/api";
+
+module.exports = apiUrl;
